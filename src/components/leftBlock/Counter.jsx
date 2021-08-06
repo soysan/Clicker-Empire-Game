515e27ca8658f57685a67bb0f43f06b80352b24b
Masayuki <soy2.san@gmail.com>
2021-08-06 23:44:25 +0900

    test color

--- src/components/leftBlock/Counter.jsx
+++ src/components/leftBlock/Counter.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { blue } from '@material-ui/core/colors'
 export default function Counter() {
   return (
     <>
-      <div className="">
+      <div style={{backgroundColor: blue[300]}}>
         <h2>1000 count</h2>
         <p>$40 per second</p>
       </div>
