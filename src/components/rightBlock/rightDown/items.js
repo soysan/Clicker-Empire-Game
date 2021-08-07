99eaf22ec8c5a95f02eca9ac53c5f07e2e954bea
Masayuki <soy2.san@gmail.com>
2021-08-07 22:22:01 +0900

    add name property

--- src/components/rightBlock/rightDown/items.js
+++ src/components/rightBlock/rightDown/items.js
@@ -1,50 +1,58 @@
 export const items = {
   "ETF Stock": {
-    max: 0,
+    name: "ETF Stock",
     price: 300000,
     capacity: "0.1%"
   },
   "ETF Bonds": {
-    max: 0,
+    name: "ETF Bonds",
     price: 300000,
     capacity: "0.07%"
   },
   "Lemonade Stand": {
+    name: "Lemonade Stand",
     max: 1000,
     price: 30000,
     capacity: "300"
   },
   "Ice Cream Truck": {
+    name: "Ice Cream Truck",
     max: 500,
     price: 100000,
     capacity: "120"
   },
   "House": {
+    name: "House",
     max: 100,
     price: 20000000,
     capacity: "32000"
   },
   "TownHouse": {
+    name: "TownHouse",
     max: 100,
     price: 40000000,
     capacity: "64000"
   },
   "Mansion": {
+    name: "Mansion",
     max: 20,
     price: 250000000,
     capacity: "500000"
   },
   "Industrial Space": {
+    name: "Industrial Space",
     max: 10,
     price: 1000000000,
     capacity: "2200000"
   },
   "Hotel Skyscraper": {
+    name: "Hotel Skyscraper",
     max: 5,
     price: 10000000000,
     capacity: "25000000"
   },
   "Bullet-Speed Sky Railway": {
+    name: "Bullet-Speed Sky Railway",
     max: 1,
     price: 10000000000000,
     capacity: "30000000000"
