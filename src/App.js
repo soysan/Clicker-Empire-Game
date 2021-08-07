890fc46562598a3492c3caf38424a97be98204fd
Masayuki <soy2.san@gmail.com>
2021-08-07 09:06:39 +0900

    delete App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Grid } from '@material-ui/core';
-import './App.css';
 import LeftBlock from './components/leftBlock/LeftBlock';
 import Status from './components/rightBlock/rightUp/Status';
 import { blue } from '@material-ui/core/colors'
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: blue[700] }}>
+    <div style={{ backgroundColor: blue[700] }}>
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <LeftBlock />
