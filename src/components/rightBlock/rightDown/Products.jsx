7e0ab355b27f5ae551c9e501c4b360e791732e8d
Masayuki <soy2.san@gmail.com>
2021-08-07 22:22:33 +0900

    poper to card

--- src/components/rightBlock/rightDown/Products.jsx
+++ src/components/rightBlock/rightDown/Products.jsx
@@ -1,22 +1,29 @@
-import { Grid, Popper } from '@material-ui/core'
+import { Grid, Card, CardContent, } from '@material-ui/core'
 import React from 'react'
-import ProductItem from './ProductDetail';
+import ProductItem from './ProductItem';
+import { items } from './items.js'
+import { green } from '@material-ui/core/colors';
 
 export default function Products() {
-  const products = ["ETF Stock","ETF Bonds", "Lemonade Stand", "Ice Cream Truck", "House", "TownHouse", "Mansion", "Industrial Space", "Hotel Skyscraper", "Bullet-Speed Sky Railway"]
+  const products = ["ETF Stock", "ETF Bonds", "Lemonade Stand", "Ice Cream Truck", "House", "TownHouse", "Mansion", "Industrial Space", "Hotel Skyscraper", "Bullet-Speed Sky Railway"]
+
   return (
     <>
       <Grid
         container
+        direction="column"
+        style={{ overflow: true, height: 400 }}
+        spacing={0}
       >
-        <Popper
-          placement="top-start"
-          disablePortal={false}
-        >
-          {products.map((product) => (
-            <ProductItem key={product} name={product} />
-          ))}
-        </Popper>
+        {products.map((product) => (
+          <Grid key={product} item xs={1}>
+            <Card style={{ backgroundColor: green[200], width: 400 }}>
+              <CardContent>
+                <ProductItem itemData={items[product]} />
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </>
   )
