37ce04c28bd49a6f9a9836400ac1971cad28a016
Masayuki <soy2.san@gmail.com>
2021-08-10 13:10:18 +0900

    fixed position

--- src/components/leftBlock/Counter.jsx
+++ src/components/leftBlock/Counter.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Grid } from '@material-ui/core';
 import { blue } from '@material-ui/core/colors'
 export default function Counter() {
   return (
     <>
-      <div style={{backgroundColor: blue[300]}}>
+      <Grid
+        style={{ backgroundColor: blue[300], }}
+        container
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+      >
         <h2>1000 count</h2>
         <p>$40 per second</p>
-      </div>
+      </Grid>
     </>
   )
 }
