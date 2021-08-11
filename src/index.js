10bd7bca82dd5f3b34fbf64457fd8adcefbb4854
Masayuki <soy2.san@gmail.com>
2021-08-12 00:17:38 +0900

    createContext

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const InitialState = {
+  userName: 'Cheater',
+  password: 'Cheater',
+}
+const AuthContext = createContext(InitialState);
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContext.Provider value={{AuthContext}}>
+      <App />
+    </AuthContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
