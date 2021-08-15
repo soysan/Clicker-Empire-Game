41d6d190ed12cf4fb2b53a7865cb47d7e86a052e
Masayuki <soy2.san@gmail.com>
2021-08-15 23:38:23 +0900

    add context

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Grid } from '@material-ui/core';
 import LeftBlock from '../components/leftBlock/LeftBlock';
 import RightBlock from '../components/rightBlock/RightBlock';
 import Options from '../components/rightBlock/Options';
+import { UserContext } from '../App';
 
 export default function MainPage() {
+  const user = useContext(UserContext);
+
   return (
     <div>
       <Grid container spacing={3}>
         <Grid item xs={6}>
-          <LeftBlock />
+          <LeftBlock param={user} />
         </Grid>
         <Grid item xs={6}>
           <RightBlock />
