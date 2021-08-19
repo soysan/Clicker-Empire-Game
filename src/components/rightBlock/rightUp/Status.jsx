b0d876071a0081488eb60a9c4fb54c412f3baffd
Masayuki <soy2.san@gmail.com>
2021-08-19 11:23:37 +0900

    effected by click and count

--- src/components/rightBlock/rightUp/Status.jsx
+++ src/components/rightBlock/rightUp/Status.jsx
@@ -1,11 +1,35 @@
 import { Grid } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../App.js';
 import Block from './Block';
 
 export default function Status() {
   const userData = useContext(UserContext);
+  const [age, setAge] = useState(userData.age);
+  const [days, setDays] = useState(userData.days);
+  const [amountMoney, setAmountMoney] = useState(userData.money);
+
+  const birthdayOfUser = () => {
+    if (days === 365) setAge(age + 1);
+    if (age !== 20 && days % 365 === 0) {
+      userData.age += 1;
+      setAge(age + 1);
+    }
+  }
+  const moveMoney = () => {
+    setAmountMoney(userData.money)
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDays(days + 1);
+      userData.days += 1;
+      birthdayOfUser();
+      moveMoney();
+    }, 1000);
+    return () => { clearInterval(intervalId) };
+  }, [userData, days, age, amountMoney, birthdayOfUser, moveMoney]);
 
   return (
     <>
@@ -18,9 +42,9 @@ export default function Status() {
         }}
       >
         <Block name={userData.name} />
-        <Block name={userData.age} />
+        <Block name={age} />
         <Block name={userData.days} />
-        <Block name={userData.money} />
+        <Block name={amountMoney} />
       </Grid>
     </>
   )
