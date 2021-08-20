399220a594b3257d8658b8efb56d350b56e54489
Masayuki <soy2.san@gmail.com>
2021-08-21 00:20:31 +0900

    color theme

---
+++ src/color.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@material-ui/core";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#a7c0cd',
+      main: '#78909c',
+      dark: '#4b636e',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ffd95b',
+      main: '#ffa726',
+      dark: '#c77800',
+      contrastText: '#000'
+    }
+  }
+});
