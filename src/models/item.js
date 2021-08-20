6fb5ac49c3cc245894afe3408c9ff967b2c52ed7
Masayuki <soy2.san@gmail.com>
2021-08-21 02:16:51 +0900

    added img

--- src/models/item.js
+++ src/models/item.js
@@ -1,5 +1,5 @@
 export default class Item {
-  constructor(name, type, maxVal, desc, price, effect, count) {
+  constructor(name, type, maxVal, desc, price, effect, count, img) {
     this.name = name;
     this.type = type;
     this.maxVal = maxVal;
@@ -8,5 +8,6 @@ export default class Item {
     this.effect = effect;
     this.count = count;
     this.isOpen = false;
+    this.img = img
   }
 }
