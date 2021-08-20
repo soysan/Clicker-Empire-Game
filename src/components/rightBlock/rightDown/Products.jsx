87d9c56225698b21ab2d134abb1aac5e3705683b
Masayuki <soy2.san@gmail.com>
2021-08-21 02:17:10 +0900

    color theme

--- src/components/rightBlock/rightDown/Products.jsx
+++ src/components/rightBlock/rightDown/Products.jsx
@@ -2,7 +2,7 @@ import { Grid, Card, CardContent } from '@material-ui/core'
 import React from 'react'
 import ProductItem from './ProductItem';
 import { names } from '../../../context/items';
-import { green, grey } from '@material-ui/core/colors';
+import { theme } from '../../../color';
 
 export default function Products() {
   console.log('product open')
@@ -13,8 +13,8 @@ export default function Products() {
         style={{
           overflowY: 'scroll',
           height: 500,
-          backgroundColor: grey[600],
-          // scrollbar hide?
+          backgroundColor: theme.palette.primary.main,
+          borderRadius: '5px',
         }}
         spacing={2}
       >
@@ -24,7 +24,11 @@ export default function Products() {
             item
             container
           >
-            <Card style={{ backgroundColor: green[200], width: '100vw' }} >
+          <Card
+              style={{
+                backgroundColor: theme.palette.secondary.main,
+                width: '100vw',
+              }} >
               <CardContent>
                 <ProductItem name={product} />
               </CardContent>
