c965c393540b59f9646ce1b7cc3844d7291562e9
Masayuki <soy2.san@gmail.com>
2021-08-20 19:06:04 +0900

    useReducer

--- src/components/rightBlock/rightDown/ProductDetail.jsx
+++ src/components/rightBlock/rightDown/ProductDetail.jsx
@@ -1,15 +1,16 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useReducer, useRef } from 'react'
 import { Button, Typography, Grid, Box } from '@material-ui/core'
 import { UserContext } from '../../../context/user';
+import { Reducer } from '../../../reducer/Reducer';
 
 export default React.forwardRef(function ProductDetail(props, ref){
   const item = props.itemData;
   const user = useContext(UserContext);
   const numRef = useRef(ref);
+  const [userState, dispatch] = useReducer(Reducer, user);
 
   const EffectShoppingHandler = () => {
     const times = Number(numRef.current.value);
-    console.log(item)
     if (item.maxVal === 0) return alert("We are out of stock. You hold everything")
     if (times > item.maxVal) return alert("We don't have the amount");
     if (times <= 0) return alert("Please type how many you want.");
@@ -17,34 +18,8 @@ export default React.forwardRef(function ProductDetail(props, ref){
     const effectAmount = times * item.price;
     if (effectAmount > user.money) return alert("You don't have enough money");
 
-    switch (item.type) {
-      case 'ability':
-        user.items.push(item.name);
-        user.money -= effectAmount;
-        user.addPerClick += item.effect * times;
-        item.maxVal -= times;
-        break;
-      case 'realEstate':
-        user.items.push(item.name);
-        user.money -= effectAmount;
-        user.addPerSec += item.effect * times;
-        item.maxVal -= times;
-        break;
-      default:
-        user.items.push(item.name);
-        user.money -= effectAmount;
-        if (item.name === 'ETF Stock') item.price += Math.floor(item.price * 0.1 * times);
-        user.stock += times;
-        let total = 0;
-        if (item.name === 'ETF Stock') {
-          for (let i = 0; i < times; i++) {
-            total += parseInt(item.price * Math.pow(1 + item.effect, i));
-          }
-        }
-        user.addPerSec += total;
-    }
+    dispatch({state: userState, type: item.type, amount: effectAmount, times: times, item: item})
   }
-
 
   return (
     <Box>
--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,22 +1,42 @@
 
 export function Reducer(state, action) {
+  const effect = action.item.effect;
+  const amount = action.amount;
+  const times = action.times;
+  const max = action.item.maxVal;
+
   switch (action.type) {
-    case 'INCREASE':
-      return {
+    case 'ability':
+      return ({
         ...state,
-        count: state.count + 1
-      }
-    case 'RESET':
-      return {
+        money: state.money - amount,
+        addPerClick: state.addPerClick += effect * times,
+      }, {
+        ...action.item,
+        maxVal: max - times,
+      })
+    case 'realEstate':
+      return ({
         ...state,
-        count: 0,
-      }
-    case 'OPEN_ITEM':
-      return {
-        ...state,
-        isOpen: !state.isOpen,
-      }
+        money: state.money - amount,
+        addPerSec: state.addPerSec + effect * times,
+      }, {
+        ...action.item,
+        maxVal: max - times,
+      })
     default:
-      return state;
+      state.money -= amount;
+      const name = action.item.name;
+      if (name === 'ETF Stock') {
+        action.item.price += Math.floor(action.item.price * 0.1 * action.times);
+        let total = 0;
+        for (let i = 0; i < action.times; i++) {
+          total += parseInt(action.item.price * Math.pow(1 + effect, i));
+        }
+        state.addPerSec += total;
+        state.stock += times;
+      } else {
+        state.addPerSec += effect * times;
+      }
   }
 }
