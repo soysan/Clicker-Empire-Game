78fa0f769ac30d2e11a3bd5428bcd798181a22f9
Masayuki <soy2.san@gmail.com>
2021-08-21 23:57:13 +0900

    add routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { blue } from '@material-ui/core/colors'
+import { theme } from './color';
 import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
-import { useContext } from 'react';
-import { AuthContext } from './index';
+import { UserContext } from './context/user';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import User from './models/user';
 
 function App() {
-  // const { user } = useContext(AuthContext);
+  const InitialUser = new User('',20,0,50000);
 
   return (
-    <div style={{
-      backgroundColor: blue[700],
-      margin: 30,
-      padding: 20
-    }}>
-      <AuthPage />
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <div style={{
+        backgroundColor: theme.palette.primary.dark,
+        padding: '10px',
+        margin: '10px 10px'
+      }}>
+        <Switch>
+          <UserContext.Provider value={InitialUser}>
+            <Route exact path='/' render={() => <AuthPage />} />
+            <Route path='/main' render={() => <MainPage />} />
+          </UserContext.Provider>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
